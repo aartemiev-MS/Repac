9cce4f695bcec2662398fc7dad0b04dfea80cd90
Sasha Artemiev <sasha@magnificentsystems.com>
2020-04-11 22:00:37 +0300

    First three pages coded

--- Repac/Repac/MainPage.xaml.cs
+++ Repac/Repac/MainPage.xaml.cs
@@ -13,7 +13,11 @@ namespace Repac
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
-        Slides CurrentSlide { get; set; }
+        Slides CurrentSlide { get; set; } = Slides.First;
+        int ScannedProducts { get; set; } = 0;
+        int ProductsCredit { get; set; } = 0;
+        bool EditingItemsQuantityMode { get; set; } = false;
+
         enum Slides
         {
             First,
@@ -24,7 +28,6 @@ namespace Repac
         public MainPage()
         {
             InitializeComponent();
-            CurrentSlide = Slides.First;
             FirstSlideActivate();
         }
 
@@ -65,6 +68,7 @@ namespace Repac
             FirstScreen.IsVisible = true;
             SecondScreen.IsVisible = false;
             ThirdScreen.IsVisible = false;
+            ButtonAdd.IsVisible = false;
         }
         private void SecondSlideActivate()
         {
@@ -74,6 +78,7 @@ namespace Repac
             FirstScreen.IsVisible = false;
             SecondScreen.IsVisible = true;
             ThirdScreen.IsVisible = false;
+            ButtonAdd.IsVisible = true;
         }
         private void ThirdSlideActivate()
         {
@@ -83,6 +88,99 @@ namespace Repac
             FirstScreen.IsVisible = false;
             SecondScreen.IsVisible = false;
             ThirdScreen.IsVisible = true;
+            ButtonAdd.IsVisible = true;
+        }
+
+        private void AddProductCredit(object sender, EventArgs e)
+        {
+            ProductsCredit += 1;
+            CounterLabelRight.Text = this.ProductsCredit.ToString();
+
+            if (ScannedProducts <= ProductsCredit)
+            {
+                CounterLabelRight.TextColor = Color.Black;
+                CreditsErrorLabel.IsVisible = false;
+
+                Footer.IsVisible = true;
+                FooterTextLabel.Text = "TAPER CARTE/TEL. POUR COMPLÉTER LA TRANSACTION.";
+                FooterTextLabel.TextColor = AddCreditButton.BackgroundColor;
+            }
+
+        }
+
+        private void FirstScreen_Tapped(object sender, EventArgs e)
+        {
+            SecondSlideActivate();
+        }
+
+        private void FooterIcon_Tapped(object sender, EventArgs e)
+        {
+            switch (CurrentSlide)
+            {
+                case Slides.Second:
+                    ThirdSlideActivate();
+                    break;
+                case Slides.Third:
+                    //for forth slide
+                    break;
+            }
+        }
+
+        private void ButtonAdd_Clicked(object sender, EventArgs e)
+        {
+            ScannedProducts += 1;
+            CounterLabel.Text = this.ScannedProducts.ToString();
+            CounterLabelLeft.Text = this.ScannedProducts.ToString();
+
+            if (ScannedProducts > ProductsCredit)
+            {
+                CounterLabelRight.TextColor = Color.DarkRed;
+                CreditsErrorLabel.IsVisible = true;
+            }
+            else
+            {
+                CounterLabelRight.TextColor = Color.Black;
+                CreditsErrorLabel.IsVisible = false;
+            }
+        }
+
+        private void ButtonSub_Clicked(object sender, EventArgs e)
+        {
+            ScannedProducts -= 1;
+            CounterLabel.Text = this.ScannedProducts.ToString();
+            CounterLabelLeft.Text = this.ScannedProducts.ToString();
+
+            if (ScannedProducts > ProductsCredit)
+            {
+                CounterLabelRight.TextColor = Color.DarkRed;
+                CreditsErrorLabel.IsVisible = true;
+            }
+            else
+            {
+                CounterLabelRight.TextColor = Color.Black;
+                CreditsErrorLabel.IsVisible = false;
+            }
+        }
+
+        private void RepacLogo_Tapped(object sender, EventArgs e)
+        {
+            if (CurrentSlide == Slides.Second)
+            {
+                EditingItemsQuantityMode = !EditingItemsQuantityMode;
+                
+                if (EditingItemsQuantityMode)
+                {
+                    ButtonAdd.Opacity = 100;
+                    ItemsAdjustmentStack.Opacity = 100;
+                    ItemsScannesGrid.Opacity = 0;
+                }
+                else
+                {
+                    ButtonAdd.Opacity = 0;
+                    ItemsAdjustmentStack.Opacity = 0;
+                    ItemsScannesGrid.Opacity = 100;
+                }
+            }
         }
     }
 }
