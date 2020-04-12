fa9fc7e666751d21281392c12ac4c57f11e2d6dc
Sasha Artemiev <sasha@magnificentsystems.com>
2020-04-12 03:56:13 +0300

    Four pages integrated and adjusted

--- Repac/Repac/MainPage.xaml.cs
+++ Repac/Repac/MainPage.xaml.cs
@@ -22,7 +22,8 @@ namespace Repac
         {
             First,
             Second,
-            Third
+            Third,
+            Fourth
         }
 
         public MainPage()
@@ -31,66 +32,7 @@ namespace Repac
             FirstSlideActivate();
         }
 
-        private void ButtonLeft_Clicked(object sender, EventArgs e)
-        {
-            switch (CurrentSlide)
-            {
-                case Slides.First:
-                    break;
-                case Slides.Second:
-                    FirstSlideActivate();
-                    break;
-                case Slides.Third:
-                    SecondSlideActivate();
-                    break;
-            }
-        }
-        private void ButtonRight_Clicked(object sender, EventArgs e)
-        {
-            switch (CurrentSlide)
-            {
-                case Slides.First:
-                    SecondSlideActivate();
-                    break;
-                case Slides.Second:
-                    ThirdSlideActivate();
-                    break;
-                case Slides.Third:
-                    break;
-            }
-        }
-
-        private void FirstSlideActivate()
-        {
-            CurrentSlide = Slides.First;
-
-            Footer.IsVisible = false;
-            FirstScreen.IsVisible = true;
-            SecondScreen.IsVisible = false;
-            ThirdScreen.IsVisible = false;
-            ButtonAdd.IsVisible = false;
-        }
-        private void SecondSlideActivate()
-        {
-            CurrentSlide = Slides.Second;
-
-            Footer.IsVisible = true;
-            FirstScreen.IsVisible = false;
-            SecondScreen.IsVisible = true;
-            ThirdScreen.IsVisible = false;
-            ButtonAdd.IsVisible = true;
-        }
-        private void ThirdSlideActivate()
-        {
-            CurrentSlide = Slides.Third;
-
-            Footer.IsVisible = false;
-            FirstScreen.IsVisible = false;
-            SecondScreen.IsVisible = false;
-            ThirdScreen.IsVisible = true;
-            ButtonAdd.IsVisible = true;
-        }
-
+        #region "Events"
         private void AddProductCredit(object sender, EventArgs e)
         {
             ProductsCredit += 1;
@@ -99,11 +41,9 @@ namespace Repac
             if (ScannedProducts <= ProductsCredit)
             {
                 CounterLabelRight.TextColor = Color.Black;
-                CreditsErrorLabel.IsVisible = false;
+                CreditsError.Opacity = 0;
 
                 Footer.IsVisible = true;
-                FooterTextLabel.Text = "TAPER CARTE/TEL. POUR COMPLÉTER LA TRANSACTION.";
-                FooterTextLabel.TextColor = AddCreditButton.BackgroundColor;
             }
 
         }
@@ -121,7 +61,7 @@ namespace Repac
                     ThirdSlideActivate();
                     break;
                 case Slides.Third:
-                    //for forth slide
+                    FourthSlideActivate();
                     break;
             }
         }
@@ -135,30 +75,33 @@ namespace Repac
             if (ScannedProducts > ProductsCredit)
             {
                 CounterLabelRight.TextColor = Color.DarkRed;
-                CreditsErrorLabel.IsVisible = true;
+                CreditsError.Opacity = 100;
             }
             else
             {
                 CounterLabelRight.TextColor = Color.Black;
-                CreditsErrorLabel.IsVisible = false;
+                CreditsError.Opacity = 0;
             }
         }
 
         private void ButtonSub_Clicked(object sender, EventArgs e)
         {
-            ScannedProducts -= 1;
-            CounterLabel.Text = this.ScannedProducts.ToString();
-            CounterLabelLeft.Text = this.ScannedProducts.ToString();
+            if (ScannedProducts > 0)
+            {
+                ScannedProducts -= 1;
+                CounterLabel.Text = this.ScannedProducts.ToString();
+                CounterLabelLeft.Text = this.ScannedProducts.ToString();
 
-            if (ScannedProducts > ProductsCredit)
-            {
-                CounterLabelRight.TextColor = Color.DarkRed;
-                CreditsErrorLabel.IsVisible = true;
-            }
-            else
-            {
-                CounterLabelRight.TextColor = Color.Black;
-                CreditsErrorLabel.IsVisible = false;
+                if (ScannedProducts > ProductsCredit)
+                {
+                    CounterLabelRight.TextColor = Color.DarkRed;
+                    CreditsError.Opacity = 100;
+                }
+                else
+                {
+                    CounterLabelRight.TextColor = Color.Black;
+                    CreditsError.Opacity = 0;
+                }
             }
         }
 
@@ -167,20 +110,111 @@ namespace Repac
             if (CurrentSlide == Slides.Second)
             {
                 EditingItemsQuantityMode = !EditingItemsQuantityMode;
-                
+
                 if (EditingItemsQuantityMode)
                 {
                     ButtonAdd.Opacity = 100;
-                    ItemsAdjustmentStack.Opacity = 100;
-                    ItemsScannesGrid.Opacity = 0;
+                    ButtonSubstract.Opacity = 100;
+                    ItemsAdjustmentLabel.Opacity = 100;
+                    ItemsScannesLabel.Opacity = 0;
                 }
                 else
                 {
                     ButtonAdd.Opacity = 0;
-                    ItemsAdjustmentStack.Opacity = 0;
-                    ItemsScannesGrid.Opacity = 100;
+                    ButtonSubstract.Opacity = 0;
+                    ItemsAdjustmentLabel.Opacity = 0;
+                    ItemsScannesLabel.Opacity = 100;
                 }
             }
         }
+
+        #endregion
+
+        #region "Functions"
+        private void FirstSlideActivate()
+        {
+            CurrentSlide = Slides.First;
+
+            FooterTextLabel.Text = "TAPER CARTE/TEL. POUR VOUS IDENTIFIER.";
+            FooterTextLabel.TextColor = Color.Black;
+            Footer.IsVisible = false;
+
+            InviteIcon.IsVisible = true;
+            PhilippeAvatar.IsVisible = false;
+
+            FirstScreen.IsVisible = true;
+            SecondScreen.IsVisible = false;
+            ThirdScreen.IsVisible = false;
+            FourthScreen.IsVisible = false;
+        }
+        private void SecondSlideActivate()
+        {
+            CurrentSlide = Slides.Second;
+
+            CounterLabel.Text = this.ScannedProducts.ToString();
+            
+            ButtonAdd.Opacity = 0;
+            ButtonSubstract.Opacity = 0;
+            ItemsAdjustmentLabel.Opacity = 0;
+            ItemsScannesLabel.Opacity = 100;
+
+            Footer.IsVisible = true;
+            InviteIcon.IsVisible = true;
+            PhilippeAvatar.IsVisible = false;
+
+            FirstScreen.IsVisible = false;
+            SecondScreen.IsVisible = true;
+            ThirdScreen.IsVisible = false;
+            FourthScreen.IsVisible = false;
+        }
+        private void ThirdSlideActivate()
+        {
+            CurrentSlide = Slides.Third;
+
+            CounterLabelLeft.Text = this.ScannedProducts.ToString();
+            CounterLabelRight.Text = this.ProductsCredit.ToString();
+
+            Footer.IsVisible = false;
+            InviteIcon.IsVisible = false;
+            PhilippeAvatar.IsVisible = true;
+
+            FirstScreen.IsVisible = false;
+            SecondScreen.IsVisible = false;
+            ThirdScreen.IsVisible = true;
+            FourthScreen.IsVisible = false;
+
+            FooterTextLabel.Text = "TAPER CARTE/TEL. POUR COMPLÉTER LA TRANSACTION.";
+            FooterTextLabel.TextColor = AddCreditButton.BackgroundColor;
+            Footer.IsVisible = ScannedProducts <= ProductsCredit ? true : false;
+        }
+        private void FourthSlideActivate()
+        {
+            CurrentSlide = Slides.Fourth;
+
+            Footer.IsVisible = false;
+            InviteIcon.IsVisible = false;
+            PhilippeAvatar.IsVisible = true;
+
+            FirstScreen.IsVisible = false;
+            SecondScreen.IsVisible = false;
+            ThirdScreen.IsVisible = false;
+            FourthScreen.IsVisible = true;
+        }
+
+        private void NullifyCycle()
+        {
+            ScannedProducts = 0;
+            ProductsCredit = 0;
+            EditingItemsQuantityMode = false;
+
+            FirstSlideActivate();
+        }
+
+        private void MerciTapped(object sender, EventArgs e)
+        {
+            NullifyCycle();
+        }
+
+        #endregion
     }
 }
